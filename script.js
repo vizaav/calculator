b7e883c26613032690d513a5dc756e0e90a5b1df
Iza <lilamovie@wp.pl>
2023-11-02 21:30:12 +0100

    feat: addition works

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const calculator = document.querySelector("#calculator");
 const calculatorDisplay = calculator.querySelector("input");
 const calculatorKeys = calculator.querySelectorAll("button[data-value]");
+const calculatorOperators = calculator.querySelectorAll("button[data-op]");
 let displayValue = "0";
+
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = null;
 
 function updateDisplay(newChar) {
   if(displayValue === "0") {
@@ -23,3 +28,26 @@ for (const button of calculatorKeys) {
         updateDisplay(buttonValue);
     })
 }
+
+for (const button of calculatorOperators) {
+    const buttonValue = button.getAttribute("data-op");
+    button.addEventListener("click", () => {
+        console.log(buttonValue);
+        if(buttonValue == "eq"){
+            secondOperand = parseFloat(calculatorDisplay.value);
+            if(operator === "+") {
+                displayValue = firstOperand + secondOperand;
+                calculatorDisplay.value = displayValue;
+                return;
+            }
+        }
+        if(buttonValue === "add") {
+            operator = "+";
+            firstOperand = parseFloat(calculatorDisplay.value);
+            displayValue = "0";
+            calculatorDisplay.value = displayValue;
+            return;
+        }
+    })
+}
+
